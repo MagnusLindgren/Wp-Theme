b0c12dbe4833c859b5e490481e750d4ef041860a
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-10-27 09:42:19 +0200

    Lots of CSS

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </div>
-<footer class="border-top footer text-muted">
-    <div class="container text-center">
+<footer class="border-top footer text-center">
+    <div class="container">
         &copy; 2021 - Magnus Lindgren - <a href="https://github.com/MagnusLindgren?tab=repositories">My GitHub</a>
     </div>
 </footer>
--- front-page.php
+++ front-page.php
@@ -1,8 +1,18 @@
 <?php
 get_header();
+
+
+$title = get_field('title');
+$description = get_field('description');
+$link = get_field('page_link');
+$heroImageArray = get_field('image');
 ?>
 
-<h1><?php echo the_title(); ?></h1>
+<h1 class="display-1"><?php echo $title; ?></h1>
+<?php echo $description; ?>
+<button type="button" class="btn btn-outline-light">
+<a style="text-decoration: none; color: white"href="<?php echo $link['url']; ?>">
+<?php echo $link['title']; ?>
 
 <?php
 get_footer();
