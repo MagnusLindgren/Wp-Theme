4c5905a5c2662197567d8d420533fe7e9edeea90
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-10-30 12:28:13 +0200

    Added mail link to creator

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <footer class="footer">
     <div class="container">
         <span class="text-muted">
-            &copy; 2021 - Magnus Lindgren - <a href="https://github.com/MagnusLindgren?tab=repositories">My GitHub</a>
+            &copy; 2021 - <a href="mailto:magnus.c.lindgren@gmail.com">Magnus Lindgren</a> - <a href="https://github.com/MagnusLindgren?tab=repositories">My GitHub</a>
         </span>        
     </div>
 </footer>
