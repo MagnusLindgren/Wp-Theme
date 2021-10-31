5e96e558aa5d9c947540faebd12efe27d1637875
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-10-31 11:53:19 +0100

    About page basics are done

--- page.php
+++ page.php
@@ -1,8 +1,29 @@
 <?php
 get_header();
-?>
 
-    <h1><?php echo "This is the page.php, here you find about and contact"; ?></h1>
+$about_title = get_field('about_title');
+$about_desc = get_field('about_description');
+$aboutImageArray = get_field('about_image');
+$aboutImage = $aboutImageArray['sizes']['medium'];
+$aboutEmail = get_field('about_email');
+?>
+<div class="container">
+    <h1 class="display-1"><?php echo $about_title; ?></h1>
+    <div class="row">
+        <div class="col-sm">
+            <?php echo $about_desc; ?>
+        </div>
+        <div class="col-sm">
+            <img class=" img-fluid front-page-image" src="<?php echo $aboutImage; ?>">
+            <div class="row">
+        Contact Info:
+    <a href="mailto:<?php echo $aboutEmail; ?>"><?php echo $aboutEmail; ?></a>
+    </div>
+        </div>
+        
+    </div>
+    
+</div>
 
 <?php
 get_footer();
